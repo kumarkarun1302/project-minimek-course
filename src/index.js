757f9cc29e5dcd5f2ebf250ef7415a10e0745c89
Afshin Mokhtari <afshinator@hotmail.com>
2018-01-11 18:15:23 -0800

    Add Redux devtools extension hooks

--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import {Provider} from "react-redux";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 
-import './index.css';
-import App from './App';
+import "./index.css";
 
-// import registerServiceWorker from './registerServiceWorker';
 import configureStore from "./store/configureStore";
-
 const store = configureStore();
 
-ReactDOM.render(
-    <Provider store={store}>
-        <App />
-    </Provider>,
-    document.getElementById('root')
-);
+// Save a reference to the root element for reuse
+const rootEl = document.getElementById("root");
 
-// registerServiceWorker();
+// Create a reusable render method that we can call more than once
+let render = () => {
+  // Dynamically import our main App component, and render it
+  const App = require("./App").default;
+
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    rootEl
+  );
+};
+
+if (process.env.NODE_ENV !== "production") {
+  if (module.hot) {
+    // Support hot reloading of components.
+    // Whenever the App component file or one of its dependencies
+    // is changed, re-import the updated component and re-render it
+    module.hot.accept("./App", () => {
+      setTimeout(render);
+    });
+  }
+}
+
+render();
