ab91599cadd940d704814d03c4327758c1065042
Afshin Mokhtari <afshinator@hotmail.com>
2018-01-11 18:33:56 -0800

    Put in Semantic UI header and yank out old code, cleanup leftover files

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Header } from "semantic-ui-react";
 
-import SampleComponent from "./SampleComponent";
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <SampleComponent />
+        <div className="App-header">
+          <Header inverted as="h1">
+            Project Mini-Mek
+          </Header>
+        </div>
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Provider } from "react-redux";
 
-import "./index.css";
+// import "./index.css";
+import "semantic-ui-css/semantic.css";
 
 import configureStore from "./store/configureStore";
 const store = configureStore();
