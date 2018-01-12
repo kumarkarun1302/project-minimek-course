e699bd9d9aa6b4a3313e73ae67db869c5d97fbb5
Afshin Mokhtari <afshinator@hotmail.com>
2018-01-11 19:18:33 -0800

    Add TabBarContainer and logic so can go between tabs

--- src/App.js
+++ src/App.js
@@ -1,32 +1,34 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
 import {
     Header,
     Container,
-    Menu,
 } from "semantic-ui-react";
 
-import "./App.css";
+import TabBarContainer from "./features/tabs/TabBarContainer";
+
+
+import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <Header inverted as="h1">
-            Project Mini-Mek
-          </Header>
-        </div>
-        <Container>
-            <Menu tabular>
-                <Menu.Item name="unitInfo" active={true}>Unit Info</Menu.Item>
-                <Menu.Item name="pilots" active={false}>Pilots</Menu.Item>
-                <Menu.Item name="mechs" active={false}>Mechs</Menu.Item>
-                <Menu.Item name="unitOrganization" active={false}>Unit Organization</Menu.Item>
-            </Menu>
-        </Container>
-      </div>
-    );
-  }
+    render() {
+        const tabs = [
+            {name : "unitInfo", label : "Unit Info"},
+            {name : "pilots", label : "Pilots"},
+            {name : "mechs", label : "Mechs"},
+            {name : "unitOrganization", label : "Unit Organization"}
+        ];
+
+        return (
+            <div className="App">
+                <div className="App-header">
+                    <Header inverted as="h1">Project Mini-Mek</Header>
+                </div>
+                <Container>
+                    <TabBarContainer tabs={tabs} size="massive" />
+                </Container>
+            </div>
+        );
+    }
 }
 
 export default App;
