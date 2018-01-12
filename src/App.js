d81c8525f3d9ec53cc448ea21bf2d6cd81715485
Afshin Mokhtari <afshinator@hotmail.com>
2018-01-11 18:43:18 -0800

    Render initial hardcoded tab bar

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
 import React, { Component } from "react";
-import { Header } from "semantic-ui-react";
+import {
+    Header,
+    Container,
+    Menu,
+} from "semantic-ui-react";
 
 import "./App.css";
 
@@ -12,6 +16,14 @@ class App extends Component {
             Project Mini-Mek
           </Header>
         </div>
+        <Container>
+            <Menu tabular>
+                <Menu.Item name="unitInfo" active={true}>Unit Info</Menu.Item>
+                <Menu.Item name="pilots" active={false}>Pilots</Menu.Item>
+                <Menu.Item name="mechs" active={false}>Mechs</Menu.Item>
+                <Menu.Item name="unitOrganization" active={false}>Unit Organization</Menu.Item>
+            </Menu>
+        </Container>
       </div>
     );
   }
